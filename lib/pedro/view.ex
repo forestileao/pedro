4c2d3f8602366c1583c5b581a6e83e8839054eaf
Pedro Leão <pedroforestileao@gmail.com>
2024-02-28 02:13:13 -0300

    feature: added reflection function

--- lib/pedro/view.ex
+++ lib/pedro/view.ex
@@ -2,11 +2,16 @@ defmodule Pedro.View do
   require EEx
   alias Pedro.Controller
 
-  def render(conn, file, assigns) do
+  def render(view_module, conn, file, assigns) do
+    functions = view_module.__info__(:functions)
     contents =
       file
       |> html_file_path()
-      |> EEx.eval_file(assigns: assigns)
+      |> EEx.eval_file([assigns: assigns], [
+        functions: [
+          {view_module, functions}
+        ]
+      ])
 
     Controller.render(conn, :html, contents)
   end
